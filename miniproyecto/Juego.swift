e66ff88fdcfce63951d919dd430a18e51473bb98
Otto Colomina <otto@dccia.ua.es>
2023-10-19 23:39:13 +0200

    Deployed 7ee1421 with MkDocs version: 1.4.1

--- miniproyecto/Juego.swift
+++ miniproyecto/Juego.swift
@@ -1,99 +1,89 @@
-//Esto de momento no se usa, pero luego sí, ya que necesitaremos notificaciones
-import Foundation
-
-
 enum EstadoJuego {
-    case turnoJugador, ganaJugador, pierdeJugador, empate, noIniciado
+    //pierdeJugador sería que pierde porque la máquina está más cerca del 7.5
+    //sepasaJugador sería que pierde porque se ha pasado
+    case noIniciado, turnoJugador, ganaJugador, sepasaJugador, pierdeJugador, empate
 }
 
-//Versión simplificada de las 7 y media en la que la máquina no saca cartas una a una
-//sino que simula la jugada generando un valor al azar entre 1 y 7.5 (no se pasa nunca!)
+
+
 class Juego {
-    var baraja : Baraja!
-    var manoJugador : Mano!
+    var baraja : Baraja
+    var manoJugador : Mano
     var estado : EstadoJuego
-    var jugadaMaquina : Double = 0.0
+    var sumaMaquina : Double = 0.0
+    var sumaJugador : Double = 0.0
     
     init() {
-        self.estado = EstadoJuego.noIniciado
+        self.estado = .noIniciado
+        self.baraja = Baraja()
+        self.baraja.barajar()
+        self.manoJugador = Mano()
     }
     
     //LLamar a este método cuando se pulse el botón de "comenzar partida"
-    func comenzarPartida() {
-        self.baraja = Baraja()
-        self.baraja.barajar()
-        
-        self.manoJugador = Mano()
+    func turnoMaquina() {
         //generamos un valor al azar entre 1 y 7.5. La máquina nunca se pasa
         //primero generamos un valor entre 1 y 7
-        jugadaMaquina = Double(Int.random(in: 1...7))
+        self.sumaMaquina = Double(Int.random(in: 1...7))
         //y luego el 50% de las veces le sumamos 0.5
         if (Bool.random()) {
-            jugadaMaquina += 0.5
+            self.sumaMaquina += 0.5
         }
+        self.estado = .turnoJugador
     }
     
     //LLamar a este método cuando se pulse el botón de "Pedir carta"
-    func jugadorPideCarta() {
+    func jugadorPideCarta() -> EstadoJuego {
         if let pedida = self.baraja.repartirCarta() {
             print("Sacas \(pedida.descripcion())")
             self.manoJugador.addCarta(pedida)
-            let valorMano = self.sumarManoJugador()
-            print("Llevas \(valorMano) puntos")
-            if (valorMano>7.5) {
+            self.sumaJugador = self.sumarManoJugador()
+            print("Llevas \(self.sumaJugador) puntos")
+            if (self.sumaJugador>7.5) {
                 acabarPartida()
             }
         }
+        return self.estado
     }
     
     //LLamar a este método cuando se pulse el botón de "Plantarse"
-    func jugadorSePlanta() {
+    func jugadorSePlanta() -> EstadoJuego {
         acabarPartida()
+        return self.estado
     }
     
     
     //Métodos para uso interno de la clase, no es necesario llamarlos desde fuera
+    //Calcula quién gana, cambia el estado del juego y lo muestra en la consola con print
     private func acabarPartida() {
-        let valorMano = sumarManoJugador()
-        var mensaje = ""
-        if (valorMano>7.5) {
-            mensaje = "Te has pasado!!!, la máquina tenía \(self.jugadaMaquina)"
+        //sumar el valor de las cartas del jugador y guardarlo en sumaJugador, si no está calculado
+        self.sumaJugador = self.sumarManoJugador()
+        //calcular quién gana, en función de esta suma y la jugada de la máquina
+        if self.sumaJugador>7.5 {
+            self.estado = .sepasaJugador
+        } else if self.sumaJugador>self.sumaMaquina {
+            self.estado = .ganaJugador
+        } else  if self.sumaJugador==self.sumaMaquina{
+            self.estado = .empate
+        }
+        else {
             self.estado = .pierdeJugador
         }
-        else {
-            if (valorMano>jugadaMaquina) {
-                mensaje = "Ganas!!!, la máquina tiene \(self.jugadaMaquina)"
-                self.estado = .ganaJugador
-            }
-            else if (valorMano<jugadaMaquina) {
-                mensaje = "Pierdes!!!, la máquina tiene \(self.jugadaMaquina)"
-                self.estado = .pierdeJugador
-            }
-            else {
-                mensaje = "Empate!!!"
-                self.estado = .empate
-            }
-        }
-        print(mensaje)
-        
-        //TO-DO: FALTA enviar notificación indicando que la partida ha terminado,
-        //para que el ViewController se entere y muestre el resultado gráficamente (con UIAlertController)
     }
-    
+
     private func sumarManoJugador() -> Double {
-        var total = 0.0
+        var suma = 0.0
         for carta in self.manoJugador.cartas {
-            total += valor(de:carta)
+            switch carta.valor {
+                case 1...7:
+                    suma += Double(carta.valor)
+                case 10,11,12:
+                    suma += 0.5
+                default:
+                    break
+            }
         }
-        return total
+        return suma
     }
     
-    private func valor(de carta:Carta) -> Double {
-        if (carta.valor>=10) {
-           return 0.5
-        }
-        else {
-           return Double(carta.valor)
-        }
-    }
 }
